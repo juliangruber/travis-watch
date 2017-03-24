342a919529c4a5f93a11c70c3910f441ca6bbf83
Julian Gruber <julian@juliangruber.com>
2017-03-24 09:13:08 +0100

    refactor renderer. add header to output

--- index.js
+++ index.js
@@ -3,40 +3,39 @@
 
 const fs = require('fs')
 const Travis = require('travis-ci')
-const ora = require('ora')
-const chalk = require('chalk')
 const resolve = require('path').resolve
-const ms = require('ms')
-const spinners = require('cli-spinners')
-const differ = require('ansi-diff-stream')
-const compare = require('./lib/compare')
 const getCommit = require('git-current-commit').sync
 const gitRemoteOriginUrl = require('git-remote-origin-url')
 const parseGitHubRepoUrl = require('parse-github-repo-url')
+const render = require('./lib/render')
 
+const travis = new Travis({ version: '2.0.0' })
 const dir = resolve(process.argv[2] || '.')
 
-const sha = getCommit(dir)
-const travis = new Travis({ version: '2.0.0' })
-
-let repo
+const state = {
+  commit: { sha: getCommit(dir) },
+  repo: null,
+  build: null,
+  results: {}
+}
 
 const getRepo = (dir, cb) => {
-  if (repo) return cb(null, repo)
+  if (state.repo) return cb()
   gitRemoteOriginUrl(dir).then(url => {
-    repo = parseGitHubRepoUrl(url)
-    cb(null, repo)
+    state.repo = parseGitHubRepoUrl(url)
+    cb()
   })
 }
 
 const getBuilds = cb => {
-  getRepo(dir, (err, repo) => {
-    if (err) return cb(err)
-    travis.repos(repo[0], repo[1]).builds.get({ event_type: 'push' }, cb)
+  getRepo(dir, () => {
+    travis
+      .repos(state.repo[0], state.repo[1])
+      .builds.get({ event_type: 'push' }, cb)
   })
 }
 
-const findCommit = commits => commits.find(c => c.sha === sha)
+const findCommit = commits => commits.find(c => c.sha === state.commit.sha)
 const findBuild = (builds, commitId) =>
   builds.find(b => b.commit_id === commitId)
 
@@ -45,9 +44,14 @@ const getBuild = cb => {
     if (err) return cb(err)
     const commit = findCommit(res.commits)
     if (!commit) return getBuild(cb)
+    state.commit = commit
     const build = findBuild(res.builds, commit.id)
-    if (build) cb(null, build)
-    else getBuild(cb)
+    if (build) {
+      state.build = build
+      cb()
+    } else {
+      getBuild(cb)
+    }
   })
 }
 
@@ -58,74 +62,27 @@ const getJob = (id, cb) => {
   })
 }
 
-let diff = differ()
-diff.pipe(process.stdout)
-let frameIdx = 0
+setInterval(() => render(state), 100)
 
-const render = () => {
-  let out = ''
-
-  Object.keys(results).forEach((os, i, arr) => {
-    const versions = Object.keys(results[os]).sort(compare)
-    spinner.stop()
-
-    if (i === 0) out += '\n'
-    if (arr.length > 1) out += `${chalk.gray(os)}\n`
-
-    versions.forEach(version => {
-      const job = results[os][version]
-      out += `  ${check(job.state, spinners.dots.frames[frameIdx])} ${getJobLanguage(job)} ${version}`
-      if (job.state === 'started') {
-        out += ` ${chalk.white(`(${ms(new Date() - new Date(job.started_at))})`)}`
-      }
-      out += '\n'
-    })
-
-    out += '\n'
-  })
-
-  frameIdx = (frameIdx + 1) % spinners.dots.frames.length
-
-  diff.write(out)
-}
-
-const results = {}
-
-setInterval(render, 100)
-
-const check = (state, frame) => {
-  const out = state === 'failed'
-    ? chalk.red('×')
-    : state === 'passed'
-        ? chalk.green('✓')
-        : state === 'started' ? chalk.yellow(frame) : chalk.gray(frame)
-  return out
-}
-
-const spinner = ora('Loading build').start()
-
-const getJobLanguage = job =>
-  job.config.node_js ? 'node' : job.config.php ? 'php' : '?'
 const getJobKey = job => job.config.node_js || job.config.php
 
-getBuild((err, build) => {
+getBuild(err => {
   if (err) throw err
 
-  spinner.text = 'Loading jobs'
-  let todo = build.job_ids.length
+  let todo = state.build.job_ids.length
   let exitCode = 0
 
-  build.job_ids.forEach(jobId => {
+  state.build.job_ids.forEach(jobId => {
     const check = (err, job) => {
       if (err) throw err
-      results[job.config.os] = results[job.config.os] || {}
-      results[job.config.os][getJobKey(job)] = job
+      state.results[job.config.os] = state.results[job.config.os] || {}
+      state.results[job.config.os][getJobKey(job)] = job
       if (job.state === 'failed') exitCode = 1
       if (job.state === 'started' || job.state === 'created') {
         getJob(jobId, check)
       } else {
         if (!--todo) {
-          render()
+          render(state)
           process.exit(exitCode)
         }
       }
